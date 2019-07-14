f092c7927c4aabec74a4a2240a9457da16039385
xieliwei <xieliwei@ec.e-chemchina.com>
2019-07-14 12:31:54 +0800

    增加日志打印

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import com.alibaba.fastjson.JSON;
 import com.lw.entity.FundAccountSituationEntity;
 import com.lw.service.impl.AccountSituationServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         AccountSituationServiceImpl service = context.getBean("accountSituationServiceImpl", AccountSituationServiceImpl.class);
@@ -14,7 +19,8 @@ public class Main {
         System.out.println(list.size());
 
         for (FundAccountSituationEntity entity : list) {
-            System.out.println(entity);
+//            log.info(JSON.toJSONString(entity));
+            log.info("共有数据{}条",list.size());
         }
     }
 }
